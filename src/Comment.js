4ef6e6de882d8416227d99aa22b28f01689b89fc
Charles Butler <cdbutle@outlook.com>
2021-02-12 02:50:01 +0000

    Automatically backed up by Learn

--- src/Comment.js
+++ src/Comment.js
@@ -1,1 +1,12 @@
 //your code here
+import REACT, { Component } from 'react';
+
+export default class Comment extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
